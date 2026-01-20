81b970c5a437bbf864582fc845020a1f82bad019
Illiall <ilivanivan2009@gmail.com>
2026-01-20 16:11:51 -0600

    New Module ID

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -21,6 +21,7 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+  //New Module ID's
   public static class Drivetrain{
     public static class SwerveModule0{
       public static final int drive_id = 36;
